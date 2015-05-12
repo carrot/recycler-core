4d2559b1fb5520e50bbfaf8d05c7ec4320e53511
BrandonRomano <BrandonRRomano@gmail.com>
2015-05-12 11:36:21 -0400

    Adding in ProgressRecyclerView

---
+++ src/main/java/com/carrotcreative/recyclercore/widget/ProgressRecyclerView.java
@@ -0,0 +1,63 @@
+package com.carrotcreative.recyclercore.widget;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import com.carrotcreative.recyclercore.R;
+
+public class ProgressRecyclerView extends RelativeLayout {
+
+    private RecyclerView mRecyclerView;
+    private ProgressBar mProgressBar;
+
+    public ProgressRecyclerView(Context context)
+    {
+        this(context, null);
+    }
+
+    public ProgressRecyclerView(Context context, AttributeSet attrs)
+    {
+        this(context, attrs, 0);
+    }
+
+    public ProgressRecyclerView(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+
+        // Inflating the View
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.progress_recycler_view, this, true);
+    }
+
+    @Override
+    protected void onFinishInflate()
+    {
+        super.onFinishInflate();
+        findViews();
+    }
+
+    private void findViews()
+    {
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+    }
+
+    /** Wrapper for {@link android.support.v7.widget.RecyclerView#setLayoutManager} */
+    public void setLayoutManager(RecyclerView.LayoutManager layoutManager)
+    {
+        mRecyclerView.setLayoutManager(layoutManager);
+    }
+
+    /** Wrapper for {@link android.support.v7.widget.RecyclerView#setAdapter} */
+    public void setAdapter(RecyclerView.Adapter adapter)
+    {
+        mRecyclerView.setAdapter(adapter);
+        mProgressBar.setVisibility(GONE);
+    }
+
+}
