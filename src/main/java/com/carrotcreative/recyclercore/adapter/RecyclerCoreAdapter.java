34bf4b2f0c388c2f65a9e29dc7ffc17739dafc97
Kaushik Saurabh <roideuniverse@gmail.com>
2015-05-14 00:05:52 -0400

    Use reflection to instantiate models
    clean up the code

--- src/main/java/com/carrotcreative/recyclercore/adapter/RecyclerCoreAdapter.java
+++ src/main/java/com/carrotcreative/recyclercore/adapter/RecyclerCoreAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import com.carrotcreative.recyclercore.util.InstantiationUtil;
 import java.util.HashMap;
 import java.util.List;
 
@@ -14,35 +15,40 @@ public abstract class RecyclerCoreAdapter extends RecyclerView.Adapter<RecyclerC
     private List<RecyclerCoreModel> mModelList;
     private HashMap<Integer, RecyclerCoreModel> mRegisteredModels;
 
+    /**
+     * If the models in the adapter change, register for the new models
+     */
+    private RecyclerView.AdapterDataObserver mDataObserver = new RecyclerView.AdapterDataObserver()
+    {
+        @Override
+        public void onChanged()
+        {
+            registerModels();
+            super.onChanged();
+        }
+    };
+
     public RecyclerCoreAdapter(List<RecyclerCoreModel> modelList, Activity activity)
     {
         mModelList = modelList;
         mActivity = activity;
         mRegisteredModels = new HashMap<>();
         registerModels();
+        registerAdapterDataObserver(mDataObserver);
     }
 
-    /**
-     * In this function you should call {@link #registerModel}
-     * to register all models to this adapter
-     */
-    protected abstract void registerModels();
-
-    /**
-     * This function will register a single model to the adapter
-     */
-    protected void registerModel(RecyclerCoreModel model)
+    private void registerModels()
     {
-        int key = model.getViewType().hashCode();
-        if( ! mRegisteredModels.containsKey(key))
+        if(mModelList != null)
         {
-            mRegisteredModels.put(key, model);
-        }
-        else
-        {
-            // Throw an exception -- a model is either being registered twice, or there is
-            // a collision with the hashCodes of two models
-            throw new IllegalStateException("There was an issue registering " + model.getViewType() + " with the adapter.");
+            for(RecyclerCoreModel model: mModelList)
+            {
+                int key = model.getViewType().hashCode();
+                if( ! mRegisteredModels.containsKey(key) )
+                {
+                    mRegisteredModels.put(key, InstantiationUtil.instantiateModel(model.getClass()));
+                }
+            }
         }
     }
 
@@ -84,5 +90,4 @@ public abstract class RecyclerCoreAdapter extends RecyclerView.Adapter<RecyclerC
         String modelViewType = model.getViewType();
         return modelViewType.hashCode();
     }
-
 }
