f13c953a0afb43f7e453a3ce0a7a6fe744d43477
Kaushik Saurabh <kaushik@carrotcreative.com>
2015-10-02 13:22:48 -0400

    remove getViewType

--- src/main/java/com/carrotcreative/recyclercore/adapter/RecyclerCoreModel.java
+++ src/main/java/com/carrotcreative/recyclercore/adapter/RecyclerCoreModel.java
@@ -1,9 +1,5 @@
 package com.carrotcreative.recyclercore.adapter;
 
-public abstract class RecyclerCoreModel {
-
-    public String getViewType()
-    {
-        return getClass().getCanonicalName();
-    }
+public abstract class RecyclerCoreModel
+{
 }
