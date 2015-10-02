a2effa30038c25040589361b3c11f6ec6d92bb5c
Kaushik Saurabh <kaushik@carrotcreative.com>
2015-10-02 14:05:43 -0400

    remove instantiation model function

--- src/main/java/com/carrotcreative/recyclercore/util/InstantiationUtil.java
+++ src/main/java/com/carrotcreative/recyclercore/util/InstantiationUtil.java
@@ -13,55 +13,6 @@ public class InstantiationUtil
 {
     private InstantiationUtil() {}
 
-    /**
-     *
-     * @param modelClass The model class for which we need to instantiate an object.
-     * @return
-     */
-    public static RecyclerCoreModel instantiateModel(Class<?> modelClass)
-    {
-        String className = modelClass.getCanonicalName();
-        Constructor<?>[] consArray = modelClass.getConstructors();
-        Constructor noArgConstructor = null;
-        for (Constructor constructor : consArray)
-        {
-            int argCount = constructor.getParameterTypes().length;
-            if (argCount == 0)
-            {
-                noArgConstructor = constructor;
-                break;
-            }
-        }
-
-        RecyclerCoreModel modelInstance = null;
-        if (noArgConstructor != null)
-        {
-            try
-            {
-                modelInstance = (RecyclerCoreModel) noArgConstructor.newInstance();
-            }
-            catch (InstantiationException e)
-            {
-                e.printStackTrace();
-            }
-            catch (IllegalAccessException e)
-            {
-                e.printStackTrace();
-            }
-            catch (InvocationTargetException e)
-            {
-                e.printStackTrace();
-            }
-        }
-
-        if(modelInstance == null)
-        {
-            throw new IllegalArgumentException("class " + className + " does not have a public no-argument constructor!");
-        }
-
-        return modelInstance;
-    }
-
     public static RecyclerCoreController instantiateController(Class<?> controllerClass, View itemView)
     {
         String className = controllerClass.getCanonicalName();
