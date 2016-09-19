bcaab2a54802f68cdb0e3025cbeac6bf6063c922
kaushik <roideuniverse@gmail.com>
2016-09-19 17:06:21 -0400

    Add RCModel and RCController

---
+++ recycler-core/src/main/java/com/carrotcreative/recyclercore/inject/RCModel.java
@@ -0,0 +1,19 @@
+package com.carrotcreative.recyclercore.inject;
+
+import com.carrotcreative.recyclercore.adapter.RecyclerCoreController;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by kaushiksaurabh on 9/19/16.
+ */
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.TYPE)
+public @interface RCModel
+{
+    Class<? extends RecyclerCoreController> controller();
+}
