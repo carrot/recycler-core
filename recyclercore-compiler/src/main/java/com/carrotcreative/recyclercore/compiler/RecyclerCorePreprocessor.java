3c71abb78b26cd9a67b7a228cc6b8a1fc66cbf6b
kaushik <roideuniverse@gmail.com>
2016-09-19 18:04:28 -0400

    add javapoet, autoservice, autocommon dependencies

--- recyclercore-compiler/src/main/java/com/carrotcreative/recyclercore/compiler/RecyclerCorePreprocessor.java
+++ recyclercore-compiler/src/main/java/com/carrotcreative/recyclercore/compiler/RecyclerCorePreprocessor.java
@@ -1,5 +1,10 @@
 package com.carrotcreative.recyclercore.compiler;
 
+import com.google.auto.service.AutoService;
+
+import javax.annotation.processing.Processor;
+
+@AutoService(Processor.class)
 public class RecyclerCorePreprocessor
 {
 }
