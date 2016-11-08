06f5b34a3e6c777a7ae3403b1558b9335f769948
kaushik <roideuniverse@gmail.com>
2016-11-08 14:16:00 -0500

    Add comment for MirroredTypeException

--- recyclercore-compiler/src/main/java/com/carrotcreative/recyclercore/compiler/ModelDetails.java
+++ recyclercore-compiler/src/main/java/com/carrotcreative/recyclercore/compiler/ModelDetails.java
@@ -40,6 +40,27 @@ class ModelDetails
         return mControllerCanonicalName;
     }
 
+    /**
+     * When the annotation value is a Class, accessing the class value is not straightforward.
+     * During the pre processing state, Javac does not load classes that are in the source.
+     * So when we call annotation.controller() it fails with MirroredTypeException.
+     * There are two possible workarounds for this.
+     * <p>
+     * Method 1. use getAnnotationMirrors : When getAnnotationMirrors is used it returns a set of
+     * AnnotationMirror instances, one for each annotation.
+     * Then locate the correct AnnotationMirror for the required Annotation.
+     * Then extract the Annotation Value fron the mirror.
+     * <p>
+     * Method 2. Catch the MirroredTypeException. The exception contains the TypeMirror of the
+     * class we need.
+     * We use the second method.
+     *
+     * ref : https://blog.retep.org/2009/02/13/getting-class-values-from-annotations-in-an-annotationprocessor/
+     *
+     * @param typeElement Represents a class or Interface program element
+     * @param typesUtil   Provides utility methods for operating on types.
+     * @return new instance of ModelDetails class
+     */
     static ModelDetails fromTypeElement(TypeElement typeElement, Types typesUtil)
     {
         ClassName clsName = ClassName.get(typeElement);
